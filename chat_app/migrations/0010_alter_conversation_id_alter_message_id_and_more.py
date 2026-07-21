dd9c5212278e6d9e26fc8c747d667de22db59968
matthewmenos <minnahmat50@gmail.com>
2026-07-21 09:05:10 +0000

    Alter

--- chat_app/migrations/0010_alter_conversation_id_alter_message_id_and_more.py
+++ chat_app/migrations/0010_alter_conversation_id_alter_message_id_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chat_app', '0009_alter_pushsubscription_options_alter_conversation_id_and_more'),
+        ('chat_app', '0008_message_reply_to_and_more'),
     ]
 
     operations = [
